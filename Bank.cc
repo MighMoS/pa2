26af9fd42e0ec03d6d8646aec9e392e336211f4c
Adam Petaccia <adam@mighmos.(none)>
2008-03-05 14:53:59 -0500

    Transaction: actually be able log when it happened; add an enum to specify the type of transaction. Also sketch out a save() method.
    Add better comments.

--- Bank.cc
+++ Bank.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include "Bank.hh"
 
@@ -8,12 +9,28 @@ void Bank::log_transaction (const int id, const account_type type, const float a
 		<< std::endl;
 }
 
+// XXX: I'm not even sure what parameters this would take.
 void Bank::write_customer_report (...)
 {
 	std::cout << "STUB: WRITE CUSTOMER REPORT\n";
 }
 
-Transaction::Transaction (const int account_id, const float the_amount):
-	id (account_id), amount (the_amount)
+Transaction::Transaction (const int account_id,
+		const transaction_type its_type,
+		const float the_amount,
+		const short its_year,
+		const short its_month,
+		const short its_day):
+	id (account_id), type (its_type), amount (the_amount),
+	year (its_year), month (its_month), day (its_day)
 {
-};
+}
+
+// Rough Sketch of what it should do. In this plan save doesn't care where
+// its writing, because some higher level method can worry about that.
+void Transaction::save(std::ofstream& output)
+{
+	static const char sep = ' ';
+	output << year << sep << month << sep << day << sep << type << amount
+		<< std::endl;
+}
--- Bank.hh
+++ Bank.hh
@@ -1,5 +1,7 @@
 #ifndef BANK
 #define BANK
+#include <fstream>
+
 #include "Account.hh"
 
 class Bank {
@@ -9,15 +11,28 @@ public:
 	static void write_customer_report (...);
 };
 
-// XXX: Is this needed?
+// XXX: This isn't all, is it?
+enum transaction_type {Deposit, Withdrawal, Check};
+
+// XXX: Is this needed? And should we do this or use a time_t object?
 class Transaction {
 	const int id;
 	const float amount;
+	const short year, month, day;
+	const transaction_type type;
 
 	public:
+	
 	int get_account_id (void) const;
 	float get_amount (void) const;
-	Transaction (const int account_id, const float the_amount);
+	void save(std::ofstream& output);
+
+	Transaction (const int account_id,
+			const transaction_type its_type,
+			const float the_amount,
+			const short its_year,
+			const short its_month,
+			const short its_day);
 };
 
 #endif // BANK
