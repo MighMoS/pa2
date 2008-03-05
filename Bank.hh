c0399a34064699c1aa4853e7a669a4bcbdc3184f
Adam Petaccia <adam@mighmos.(none)>
2008-03-05 18:29:32 -0500

    Move Transaction into its own file

--- Bank.hh
+++ Bank.hh
@@ -12,29 +12,4 @@ public:
 	static void write_customer_report (...);
 };
 
-// XXX: This isn't all, is it?
-enum transaction_type {Deposit, Withdrawal, Check};
-
-// XXX: Is this needed? And should we do this or use a time_t object?
-class Transaction {
-	const unsigned int id;
-	const float amount;
-	const transaction_type type;
-
-	const short year, month, day;
-
-	public:
-	
-	unsigned int get_account_id (void) const;
-	float get_amount (void) const;
-	void save(std::ofstream& output);
-
-	Transaction (const unsigned int account_id,
-			const transaction_type its_type,
-			const float the_amount,
-			const short its_year,
-			const short its_month,
-			const short its_day);
-};
-
 #endif // BANK
---
+++ Transaction.h
@@ -0,0 +1,29 @@
+#ifndef TRANSACTION
+#define TRANSACTION
+// XXX: This isn't all, is it?
+enum transaction_type {Deposit, Withdrawal, Check};
+
+// XXX: Is this needed? And should we do this or use a time_t object?
+
+class Transaction {
+	const unsigned int id;
+	const float amount;
+	const transaction_type type;
+
+	const short year, month, day;
+
+	public:
+	
+	unsigned int get_account_id (void) const;
+	float get_amount (void) const;
+	void save(std::ofstream& output);
+
+	Transaction (const unsigned int account_id,
+			const transaction_type its_type,
+			const float the_amount,
+			const short its_year,
+			const short its_month,
+			const short its_day);
+};
+
+#endif
