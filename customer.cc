b403b3b2e90a861ef0996c00ac08faf8b77186ca
Adam Petaccia <adam@mighmos.(none)>
2008-03-31 20:38:38 -0400

    Write out data, not pointers

--- customer.cc
+++ customer.cc
@@ -76,7 +76,7 @@ void Customer::save (void) const
 	for (unsigned int i = 0; i < Accounts.size(); i++)
 		cout << Accounts[i] << " ";
 	file << endl;
-	file << address << endl;
+	file << *address << endl;
 
 	file.close();
 }
