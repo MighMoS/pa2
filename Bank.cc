d063b2487c91370dd35af2d0c870f8af5fa51ae4
Adam Petaccia <adam@mighmos.(none)>
2008-04-01 11:08:57 -0400

    Bank.cc: move Transaction implementation to its own file. Also comment out an unused function.

--- Bank.cc
+++ Bank.cc
@@ -7,12 +7,16 @@
 
 using std::endl;
 
+// I'm not sure if we want to do things like this anymore.
+// TODO: decide before pa3
+#if 0
 void Bank::log_transaction (const unsigned int id, const account_type type,
 		const float amount)
 {
 	std::cout << "STUB: LOG_TRANSACTION. ID = " << id << ", amount = " << amount 
 		<< endl;
 }
+#endif
 
 // XXX: I'm not even sure what parameters this would take.
 void Bank::write_customer_report (...)
@@ -52,43 +56,3 @@ bool Bank::init (void)
 	return true;
 }
 
-Transaction::Transaction (const unsigned int account_id,
-		const transaction_type its_type,
-		const float the_amount) :
-#if 0
-		const short its_year,
-		const short its_month,
-		const short its_day):
-	year (its_year), month (its_month), day (its_day),
-#endif
-	id (account_id), amount (the_amount), type (its_type)
-{
-}
-
-void Transaction::process (void)
-{
-	Account* acct;
-	acct = Account::get_account_by_id (id);
-	if (!acct)
-	{
-		std::cerr << "Account " << id << " doesn't exist to be processed. This shouldn't happen!\n";
-		exit (1);
-	}
-
-	// We don't have to worry about the account type, because all can
-	// add/subtract money. Also, the amount is presigned (negative if needed)
-	acct->do_transaction (amount);
-
-	delete acct;
-}
-
-#if 0
-// Rough Sketch of what it should do. In this plan save doesn't care where
-// its writing, because some higher level method can worry about that.
-void Transaction::save(std::ofstream& output)
-{
-	static const char sep = ' ';
-	output << year << sep << month << sep << day << sep << type << amount
-		<< std::endl;
-}
-#endif
